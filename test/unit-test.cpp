d4d7f788bd1da0e05be456e4f88223eb857bb852
Maarten L. Hekkelman <maarten@hekkelman.com>
2025-02-18 08:09:01 +0100

    Use Catch2 v3

--- test/unit-test.cpp
+++ test/unit-test.cpp
@@ -1,14 +1,12 @@
 #define CATCH_CONFIG_RUNNER
 
-#if CATCH22
-# include <catch2/catch.hpp>
-#else
-# include <catch2/catch_all.hpp>
-#endif
+#include "mxml.hpp"
+
+#include <catch2/catch_all.hpp>
 
 #include <filesystem>
+#include <iostream>
 
-#include "mxml.hpp"
 // #include "mxml.ixx"
 
 std::filesystem::path gTestDir = std::filesystem::current_path();
@@ -18,12 +16,7 @@ int main(int argc, char *argv[])
 	Catch::Session session; // There must be exactly one instance
 
 	// Build a new parser on top of Catch2's
-#if CATCH22
-	using namespace Catch::clara;
-#else
-	// Build a new parser on top of Catch2's
 	using namespace Catch::Clara;
-#endif
 
 	auto cli = session.cli()                                // Get Catch2's command line parser
 	           | Opt(gTestDir, "data-dir")                  // bind variable to a new option, with a hint string
