5aa84b6af254ace9c95831b0309f7d980b9a9900
Maarten L. Hekkelman <maarten@hekkelman.com>
2026-07-28 11:15:29 +0200

    Rewite xpath-test to use Catch2

--- test/xpath-test.cpp
+++ test/xpath-test.cpp
@@ -1,7 +1,15 @@
 // Copyright (c) 2026 Maarten L. Hekkelman
 // SPDX-License-Identifier: BSD-2-Clause
 
-#include <exception>
+#define CATCH_CONFIG_RUNNER
+
+#if ZEEM_USE_DATE_H
+# include <date/tz.h>
+#endif
+
+#include <catch2/catch_session.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <cstdint>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
@@ -17,6 +25,35 @@ import zeem;
 namespace fs = std::filesystem;
 
 int VERBOSE;
+
+std::filesystem::path gTestDir;
+
+int main(int argc, char *argv[])
+{
+	gTestDir = std::filesystem::current_path();
+
+	Catch::Session session; // There must be exactly one instance
+
+	// Build a new parser on top of Catch2's
+	using namespace Catch::Clara;
+
+	auto cli = session.cli();                        // Get Catch2's command line parser
+	cli |= Opt(gTestDir, "data-dir")                 // bind variable to a new option, with a hint string
+		["-D"]["--data-dir"]                         // the option names it will respond to
+		("The directory containing the data files"); // description string for the help output
+
+	// Now pass the new composite back to Catch2 so it uses that
+	session.cli(cli);
+
+	// Let Catch2 (using Clara) parse the command line
+	int returnCode = session.applyCommandLine(argc, argv);
+	if (returnCode != 0) // Indicates a command line error
+		return returnCode;
+
+	return session.run();
+}
+
+// --------------------------------------------------------------------
 
 std::ostream &operator<<(std::ostream &os, const zeem::node &n)
 {
@@ -114,72 +151,59 @@ bool run_test(const zeem::element &test)
 	return result;
 }
 
-void run_tests(const fs::path &file)
+TEST_CASE("xpath")
 {
-	if (not fs::exists(file))
-	{
-		std::cerr << "test file does not exist\n";
-		exit(1);
-	}
+	using namespace std::literals;
+	auto xmlconfFile = gTestDir / "XPath-Test-Suite" / "xpath-tests.xml";
 
-	std::ifstream input(file, std::ios::binary);
+	REQUIRE(fs::exists(xmlconfFile));
 
-	zeem::document doc;
-	input >> doc;
+	std::ifstream input(xmlconfFile, std::ios::binary);
+	zeem::document confDoc;
+	input >> confDoc;
 
-	fs::path dir = fs::absolute(file).parent_path().parent_path();
-	if (not dir.empty())
-		fs::current_path(dir);
-
-	std::string base = doc.front().get_attribute("xml:base");
+	fs::current_path(gTestDir);
+	std::string base = confDoc.front().get_attribute("xml:base");
 	if (not base.empty())
 		fs::current_path(base);
 
-	int nr_of_tests = 0, failed_nr_of_tests = 0;
-
-	for (const zeem::element *test : doc.find("//xpath-test"))
+	for (const zeem::element *test : confDoc.find("//xpath-test"))
 	{
-		++nr_of_tests;
-		if (run_test(*test) == false)
-			++failed_nr_of_tests;
-	}
+		fs::path data_file = fs::current_path() / test->get_attribute("data");
+		REQUIRE(fs::exists(data_file));
 
-	std::cout << '\n';
-	if (failed_nr_of_tests == 0)
-		std::cout << "*** No errors detected\n";
-	else
-	{
-		std::cout << failed_nr_of_tests << " out of " << nr_of_tests << " failed\n";
-		if (not VERBOSE)
-			std::cout << "Run with --verbose to see the errors\n";
-	}
-}
+		std::ifstream file(data_file, std::ios::binary);
 
-int main(int argc, char *argv[])
-{
-	using namespace std::literals;
-	fs::path xmlconfFile("XPath-Test-Suite/xpath-tests.xml");
+		zeem::document doc;
+		file >> doc;
 
-	for (int i = 1; i < argc; ++i)
-	{
-		if (argv[i] == "-v"s)
+		zeem::xpath xp(test->get_attribute("xpath"));
+
+		zeem::context context;
+		for (const zeem::element *e : test->find("var"))
+			context.set(e->get_attribute("name"), e->get_attribute("value"));
+
+		auto ns = xp.evaluate<zeem::node>(*doc.root(), context);
+
+		uint32_t expectedSize = std::stoul(test->get_attribute("expected-size"));
+
+		CHECK(ns.size() == expectedSize);
+		if (ns.size() != expectedSize)
+			std::cout << "Failed test :\n" << *test << '\n';
+
+		std::string test_attr_name = test->get_attribute("test-name");
+		std::string attr_test = test->get_attribute("test-attr");
+
+		if (not attr_test.empty())
 		{
-			++VERBOSE;
-			continue;
+			for (const zeem::node *n : ns)
+			{
+				const auto *e = dynamic_cast<const zeem::element *>(n);
+				if (e == nullptr)
+					continue;
+
+				CHECK(e->get_attribute(test_attr_name) == attr_test);
+			}
 		}
-
-		xmlconfFile = argv[i];
 	}
-
-	try
-	{
-		run_tests(xmlconfFile);
-	}
-	catch (std::exception &e)
-	{
-		std::cout << "exception: " << e.what() << '\n';
-		return 1;
-	}
-
-	return 0;
 }
