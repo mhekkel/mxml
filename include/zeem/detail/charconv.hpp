c792ad759f7040f4b72ed4c78e64313cd8ff23a6
Maarten L. Hekkelman <maarten@hekkelman.com>
2026-08-09 11:21:58 +0200

    refactored is_detected

--- include/zeem/detail/charconv.hpp
+++ include/zeem/detail/charconv.hpp
@@ -4,89 +4,64 @@
 #pragma once
 
 #ifndef ZEEM_CXX_MODULE
-# include "zeem/config.hpp"
-
 # include <charconv>
-# include <cmath>
-
-# if __has_include(<experimental/type_traits>)
-#  include <experimental/type_traits>
-# else
-#  include <type_traits>
-# endif
+# include <concepts>
+# include <type_traits>
+# include <utility>
 #endif
 
 namespace zeem
 {
 
+/// @cond
+
 namespace detail
 {
-
-#if (not defined(__cpp_lib_experimental_detect) or (__cpp_lib_experimental_detect < 201505)) and (not defined(_LIBCPP_VERSION) or _LIBCPP_VERSION < 5000)
-	// This code is copied from:
-	// https://ld2015.scusa.lsu.edu/cppreference/en/cpp/experimental/is_detected.html
-
-	template <class...>
-	using void_t = void;
-
-	namespace detail
+	template <class Default, class AlwaysVoid,
+		template <class...> class Op, class... Args>
+	struct detector : std::false_type
 	{
-		template <class Default, class AlwaysVoid,
-			template <class...> class Op, class... Args>
-		struct detector
-		{
-			using value_t = std::false_type;
-			using type = Default;
-		};
-
-		template <class Default, template <class...> class Op, class... Args>
-		struct detector<Default, void_t<Op<Args...>>, Op, Args...>
-		{
-			// Note that std::void_t is a c++17 feature
-			using value_t = std::true_type;
-			using type = Op<Args...>;
-		};
-	} // namespace detail
-
-	ZEEM_EXPORT struct nonesuch
-	{
-		nonesuch() = delete;
-		~nonesuch() = delete;
-		nonesuch(nonesuch const &) = delete;
-		void operator=(nonesuch const &) = delete;
+		using type = Default;
 	};
 
-	ZEEM_EXPORT template <template <class...> class Op, class... Args>
-	using is_detected = typename detail::detector<nonesuch, void, Op, Args...>::value_t;
-
-	ZEEM_EXPORT template <template <class...> class Op, class... Args>
-	constexpr ZEEM_INLINE bool is_detected_v = is_detected<Op, Args...>::value;
-
-	ZEEM_EXPORT template <template <class...> class Op, class... Args>
-	using detected_t = typename detail::detector<nonesuch, void, Op, Args...>::type;
-
-	ZEEM_EXPORT template <class Default, template <class...> class Op, class... Args>
-	using detected_or = detail::detector<Default, void, Op, Args...>;
-
-	ZEEM_EXPORT template <class Expected, template <class...> class Op, class... Args>
-	using is_detected_exact = std::is_same<Expected, detected_t<Op, Args...>>;
-
-	ZEEM_EXPORT template <class Expected, template <class...> class Op, class... Args>
-	constexpr ZEEM_INLINE bool is_detected_exact_v = is_detected_exact<Expected, Op, Args...>::value;
-
-#else
-
-	ZEEM_EXPORT template <template <class...> class Op, class... Args>
-	constexpr ZEEM_INLINE bool is_detected_v = std::experimental::is_detected<Op, Args...>::value;
-
-#endif
-
+	template <class Default, template <class...> class Op, class... Args>
+	struct detector<Default, std::void_t<Op<Args...>>, Op, Args...>
+		: std::true_type
+	{
+		using type = Op<Args...>;
+	};
 } // namespace detail
 
-ZEEM_EXPORT template <typename T>
+struct nonesuch
+{
+	nonesuch() = delete;
+	~nonesuch() = delete;
+	nonesuch(nonesuch const &) = delete;
+	void operator=(nonesuch const &) = delete;
+};
+
+ZEEM_EXPORT template <template <class...> class Op, class... Args>
+using is_detected = typename detail::detector<nonesuch, void, Op, Args...>;
+
+ZEEM_EXPORT template <template <class...> class Op, class... Args>
+constexpr bool is_detected_v = is_detected<Op, Args...>::value;
+
+ZEEM_EXPORT template <template <class...> class Op, class... Args>
+using detected_t = typename detail::detector<nonesuch, void, Op, Args...>::type;
+
+ZEEM_EXPORT template <class Default, template <class...> class Op, class... Args>
+using detected_or = detail::detector<Default, void, Op, Args...>;
+
+ZEEM_EXPORT template <class Expected, template <class...> class Op, class... Args>
+using is_detected_exact = std::is_same<Expected, detected_t<Op, Args...>>;
+
+ZEEM_EXPORT template <class Expected, template <class...> class Op, class... Args>
+constexpr bool is_detected_exact_v = is_detected_exact<Expected, Op, Args...>::value;
+
+template <typename T>
 using from_chars_function = decltype(std::from_chars(std::declval<const char *>(), std::declval<const char *>(), std::declval<T &>()));
 
-ZEEM_EXPORT template <typename T>
+template <typename T>
 struct std_charconv
 {
 	static std::from_chars_result from_chars(const char *a, const char *b, T &d)
@@ -96,22 +71,30 @@ struct std_charconv
 };
 
 template <typename T, typename = void>
-struct ff_charconv;
+struct ff_charconv
+{
+	static std::from_chars_result from_chars(const char *a, const char *b, T &v)
+	{
+		static_assert(not std::same_as<T, T>, "from_chars is not supported for this type");
+		std::unreachable();
+	}
+};
 
-template <typename T>
-	requires(std::is_floating_point_v<T>)
+template <std::floating_point T>
 struct ff_charconv<T>
 {
 	static std::from_chars_result from_chars(const char *a, const char *b, T &v);
 };
 
 ZEEM_EXPORT template <typename T>
-using charconv = typename std::conditional_t<detail::is_detected_v<from_chars_function, T>, std_charconv<T>, ff_charconv<T>>;
+using charconv = std::conditional_t<is_detected_v<from_chars_function, T>, std_charconv<T>, ff_charconv<T>>;
 
 ZEEM_EXPORT template <typename T>
 constexpr auto from_chars(const char *s, const char *e, T &v)
 {
 	return charconv<T>::from_chars(s, e, v);
 }
+
+/// @endcond
 
 } // namespace zeem
